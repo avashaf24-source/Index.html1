6cf9ca0b5e1383f5f22c8037f1d3f85e03ccaead
avashaf24-source <avash.af24@gmail.com>
2025-07-25 07:47:41 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('khonamon-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png',
+        '/style.css',
+        '/sound/santur.mp3'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
